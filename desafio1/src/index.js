3f831821f1808a1d0f7d87a8ac63259f1da04917
amartins25 <alexandremartins13rj@gmail.com>
2019-11-26 09:34:24 -0300

    style challenge

--- desafio1/src/index.js
+++ desafio1/src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./styles.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  return (
+    <div className="App">
+      <span className="Cep">CEP:*</span>
+      <input className="PutData"></input>
+      <span className="DataCep">Não sei meu CEP</span>
+      <span className="PutCep">Insira o CEP</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
